445929e719a3a5f1a51089f9261a98097f7b7dc6
Francisco Valle <vallecesco@gmail.com>
2021-07-09 22:36:23 -0600

    Probando mi CSS

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <header>
     <h1>Francisco José Valle Cornejo</h1>
