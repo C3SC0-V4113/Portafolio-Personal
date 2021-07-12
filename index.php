528a9a272e3a320286b49e861a1461156355d122
Francisco Valle <vallecesco@gmail.com>
2021-07-12 15:15:02 -0600

    Primeras cosas con Bootstrap

--- index.php
+++ index.php
@@ -7,29 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 </head>
-<header>
-        <h1>Francisco José Valle Cornejo</h1>
-        <h2 id="descrip">Desarrollador Web FrontEnd</p>
-    </header>
+
 <body>
-    <section>
-        <article>
-            <div class="Presentacion">
-                <h3>¿Quién soy?</h3>
-                <p>Mi nombre es Francisco José Valle Cornejo, soy un desarrollador web FrontEnd y de PHP</p>
+    <div class="container-fluid">
+    <div class="row">
+            <div class="col-12" id="header">
+                    <h1>Francisco José Valle Cornejo</h1>
+                    <h2 id="descrip">Desarrollador Web FrontEnd</p>
             </div>
-        </article>
-        <article>
-            <div>
-                <h3>Mis habilidades</h3>
-                <p>Se usar HTML,CSS, JavaScript y PHP</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <section>
+                    <article>
+                        <div class="Presentacion">
+                            <h3>¿Quién soy?</h3>
+                            <p>Mi nombre es Francisco José Valle Cornejo, soy un desarrollador web FrontEnd y de PHP</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <h3>Mis habilidades</h3>
+                            <p>Se usar HTML,CSS, JavaScript y PHP</p>
+                        </div>
+                    </article>
+                </section>
             </div>
-        </article>
-    </section>
+        </div>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+            <div class="col-12" id="footer">
+            <p>Creado con Amor por Francisco Valle 2021</p>
+            </div>
+        </div>
+    </div>
 </body>
-<footer>
-    <p>Creado con Amor por Francisco Valle 2021</p>
-</footer>
 
 </html>
