a67cb164f3fe6156eb64519937b459b52e2b5d83
Francisco Valle <vallecesco@gmail.com>
2021-07-13 17:48:04 -0600

    Empezando Composer

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 
 <body>
     <div class="container-fluid">
-    <div class="row">
+        <div class="row">
             <div class="col-12" id="header">
-                    <h1>Francisco José Valle Cornejo</h1>
-                    <h2 id="descrip">Desarrollador Web FrontEnd</p>
+                <h1>Francisco José Valle Cornejo</h1>
+                <h2 id="descrip">Desarrollador Web FrontEnd</p>
             </div>
         </div>
     </div>
@@ -24,25 +24,38 @@
             <div class="col">
                 <section>
                     <article>
-                        <div class="Presentacion">
-                            <h3>¿Quién soy?</h3>
-                            <p>Mi nombre es Francisco José Valle Cornejo, soy un desarrollador web FrontEnd y de PHP</p>
+                        <div class="row">
+                        <h3>¿Quién soy?</h3>
+                            <div class="col-12 col-md-4">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tempora alias rerum doloremque consectetur sapiente reprehenderit explicabo, accusamus magnam laudantium dignissimos sunt repudiandae quae perspiciatis nihil ipsam repellat eos distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quaerat sequi nam nemo praesentium! Voluptate, perspiciatis. Aspernatur, quaerat? Tenetur placeat quisquam quae molestiae corporis voluptates quas nisi libero enim doloribus.
+                            </div>
+                            <div class="Presentacion col-12 col-md-8">
+                                
+                                <p>Mi nombre es Francisco José Valle Cornejo</p>
+                                <p>
+                                    Soy Desarrollador Web FrontEnd salvadoreño. He estado haciendo paginas web desde mediados del año 2020 por cursos universitarios. Despues me he expandido a cursos externos en Platzi y Udemy para aumentar mi conocimiento en este campo de la programación.
+                                </p>
+                            </div>
                         </div>
                     </article>
-                    <article>
-                        <div>
-                            <h3>Mis habilidades</h3>
-                            <p>Se usar HTML,CSS, JavaScript y PHP</p>
+                </section>
+                <section>
+                <article>
+                    <div class="row">
+                    <h3>Mis Proyectos</h3>
+                        <div class="col-12">
+                            
                         </div>
+                    </div>
                     </article>
                 </section>
             </div>
         </div>
     </div>
     <div class="container-fluid">
-    <div class="row">
+        <div class="row">
             <div class="col-12" id="footer">
-            <p>Creado con Amor por Francisco Valle 2021</p>
+                <p>Creado con Amor por Francisco Valle 2021</p>
             </div>
         </div>
     </div>
