62d15654569a2453c31a9e9c8711ff8ab0f4e452
Francisco Valle <vallecesco@gmail.com>
2021-07-13 22:19:21 -0600

    Dandole más forma

--- index.php
+++ index.php
@@ -6,31 +6,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <!--Barra de Navegacion-->
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" id="navbarcool">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">¿Quien Soy?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#hab">Habilidades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacts">Contactos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--Encabezado-->
     <div class="container-fluid">
         <div class="row">
             <div class="col-12" id="header">
-                <h1>Francisco José Valle Cornejo</h1>
-                <h2 id="descrip">Desarrollador Web FrontEnd</p>
+                <div class="titulo">
+                    <h1>Francisco José Valle Cornejo</h1>
+                    <h2 id="descrip">Desarrollador Web FrontEnd</p>
+                </div>
             </div>
         </div>
     </div>
+    <!--Cuerpo-->
     <div class="container">
         <div class="row">
             <div class="col">
-                <section>
+                <section id="about">
                     <article>
                         <div class="row">
-                        <h3>¿Quién soy?</h3>
+                            <h3>Sobre mí</h3>
+                            <hr>
                             <div class="col-12 col-md-4">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque tempora alias rerum doloremque consectetur sapiente reprehenderit explicabo, accusamus magnam laudantium dignissimos sunt repudiandae quae perspiciatis nihil ipsam repellat eos distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quaerat sequi nam nemo praesentium! Voluptate, perspiciatis. Aspernatur, quaerat? Tenetur placeat quisquam quae molestiae corporis voluptates quas nisi libero enim doloribus.
+                                <img src="img/sour-moha-VfKybXJVxrk-unsplash.jpg" class="img-fluid rounded mx-auto d-block" alt="perfil">
                             </div>
                             <div class="Presentacion col-12 col-md-8">
-                                
                                 <p>Mi nombre es Francisco José Valle Cornejo</p>
                                 <p>
                                     Soy Desarrollador Web FrontEnd salvadoreño. He estado haciendo paginas web desde mediados del año 2020 por cursos universitarios. Despues me he expandido a cursos externos en Platzi y Udemy para aumentar mi conocimiento en este campo de la programación.
@@ -39,19 +71,43 @@
                         </div>
                     </article>
                 </section>
-                <section>
-                <article>
-                    <div class="row">
-                    <h3>Mis Proyectos</h3>
-                        <div class="col-12">
-                            
+                <section id="projects">
+                    <article>
+                        <div class="row">
+                            <h3>Proyectos</h3>
+                            <hr>
+                            <div class="col-12">
+
+                            </div>
                         </div>
-                    </div>
+                    </article>
+                </section>
+                <section id="hab">
+                    <article>
+                        <div class="row">
+                            <h3>Habilidades</h3>
+                            <hr>
+                            <div class="col-12">
+
+                            </div>
+                        </div>
+                    </article>
+                </section>
+                <section id="contacts">
+                    <article>
+                        <div class="row">
+                            <h3>Contactos</h3>
+                            <hr>
+                            <div class="col-12">
+
+                            </div>
+                        </div>
                     </article>
                 </section>
             </div>
         </div>
     </div>
+    <!--Footer-->
     <div class="container-fluid">
         <div class="row">
             <div class="col-12" id="footer">
@@ -60,5 +116,5 @@
         </div>
     </div>
 </body>
-
+<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 </html>
