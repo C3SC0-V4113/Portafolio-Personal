2605695cd856247ac5bc088a79115fd523f33be4
Francisco Valle <vallecesco@gmail.com>
2021-07-28 19:16:29 -0600

    Terminada Galeria de Habilidades

--- index.php
+++ index.php
@@ -1913,14 +1913,14 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-12">
-						<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+					<div class="col-12" id="div-carrusel">
+						<div id="carrousel" class="carousel slide" data-bs-ride="carousel">
 							<div class="carousel-indicators">
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+								<button type="button" data-bs-target="#carrousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+								<button type="button" data-bs-target="#carrousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+								<button type="button" data-bs-target="#carrousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+								<button type="button" data-bs-target="#carrousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+								<button type="button" data-bs-target="#carrousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
 							</div>
 							<div class="carousel-inner">
 								<div class="carousel-item">
@@ -1939,11 +1939,11 @@
 									<img src="img/certificados/diploma-economia-digital-1.png" class="d-block w-100" alt="...">
 								</div>
 							</div>
-							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+							<button class="carousel-control-prev" type="button" data-bs-target="#carrousel" data-bs-slide="prev">
 								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 								<span class="visually-hidden">Previous</span>
 							</button>
-							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+							<button class="carousel-control-next" type="button" data-bs-target="#carrousel" data-bs-slide="next">
 								<span class="carousel-control-next-icon" aria-hidden="true"></span>
 								<span class="visually-hidden">Next</span>
 							</button>
